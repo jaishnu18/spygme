886cf47fc69be45f4a3e2287f3ac59ad6247f2f7
Sagar Suman <sagarsuman1299@gmail.com>
2021-09-20 12:32:55 +0530

    read section  updated

---
+++ app/containers/ReadPracticeGradeContainer/tests/selectors.test.js
@@ -0,0 +1,7 @@
+// import { selectReadPracticeGradeContainerDomain } from '../selectors';
+
+describe('selectReadPracticeGradeContainerDomain', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
