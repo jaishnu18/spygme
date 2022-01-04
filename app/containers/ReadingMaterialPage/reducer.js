0809495bfa9c7c30c39e67227f30e39d104faef8
bbiswabasu <bbiswabasuroy2001@gmail.com>
2022-01-04 10:47:29 +0530

    ReadingMaterial Added

---
+++ app/containers/ReadingMaterialPage/reducer.js
@@ -0,0 +1,35 @@
+/*
+ *
+ * ReadingMaterialPage reducer
+ *
+ */
+import produce from 'immer';
+import { DEFAULT_ACTION, GET_RM_START, GET_RM_FAILURE, GET_RM_SUCCESS, MARK_READ_SUCCESS, MARK_READ_FAILURE, MARK_READ_START } from './constants';
+
+export const initialState = {
+  isReadingMaterialLoading: false,
+  error: undefined,
+  readingMaterialContent: undefined,
+};
+
+/* eslint-disable default-case, no-param-reassign */
+const readingMaterialPageReducer = (state = initialState, action) =>
+  produce(state, (draft) => {
+    switch (action.type) {
+      case DEFAULT_ACTION:
+        break;
+      case GET_RM_START:
+        draft.isReadingMaterialLoading = true;
+        break;
+      case GET_RM_SUCCESS:
+        draft.isReadingMaterialLoading = false;
+        draft.readingMaterialContent = action.payload;
+        break;
+      case GET_RM_FAILURE:
+        draft.isReadingMaterialLoading = false;
+        draft.readingMaterialContent = action.payload;
+        break;
+    }
+  });
+
+export default readingMaterialPageReducer;
